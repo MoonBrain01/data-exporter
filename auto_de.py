45e92140557a88215361aecb14387e597c52a1ac
Garrie Powers <g.powers@ucl.ac.uk>
2021-06-04 05:55:27 +0100

    Add option to set Search Time

--- auto_de.py
+++ auto_de.py
@@ -1,76 +1,96 @@
 import subprocess
+import sys
 import time
+from PIL.ImageOps import grayscale
 import pyautogui
 import csv
 import os
 import datetime
 
-
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 1
 
-def screen_wait(ssname, loop_limit=10):
-    loop_count = 0
-    result = None
-    while (result==None and loop_count < loop_limit ) :
-        result = pyautogui.locateOnScreen(ssname)
-        loop_count += 1
+def screen_wait(ssname, search_time=10):
+    result = pyautogui.locateOnScreen(ssname,grayscale=True,minSearchTime=search_time)
     if result==None:
-        print(f"{ssname} not found")
+        raise pyautogui.ImageNotFoundException(ssname)
     return result
 
 def open_xml(xml_file):
     pyautogui.press('alt')
     pyautogui.press('f')
     pyautogui.press('o')
-    if screen_wait(r".\screenshot\DE_open.png")==None:
-        return False
+    screen_wait(r".\screenshot\DE_open.png")
+
     pyautogui.write(xml_file)
     pyautogui.press('enter')
-    return True
+    return
 
 def export_csv(xml_file, destination):
+    # alt, e, e - CSV Export
     pyautogui.press('alt')
     pyautogui.press('e')
     pyautogui.press('e')
-    if screen_wait(r".\screenshot\DE_csv_export.png")==None:
-        return False
-    export_file = destination
-    export_file += os.path.splitext(os.path.basename(xml_file))[0]
-    today = datetime.datetime.now().strftime("_%a_%G-%m-%d").upper()
-    export_file += today + '.csv'
-    pyautogui.write(export_file)
-    pyautogui.press('enter')
-    return True
+    screen_wait(r".\screenshot\DE_csv_export.png")
 
-if __name__ == '__main__':
+    export_file = build_fname(xml_file, destination,'csv')
+    save_file(export_file)
+    return
+
+def build_fname(xml_file, destination, ext):
+    fname = destination
+    fname += os.path.splitext(os.path.basename(xml_file))[0]
+    today = datetime.datetime.now().strftime("_%a_%G-%m-%d").upper()
+    fname += today + '.' + ext
+    return fname
+
+def export_stata(xml_file, destination):
+    # alt, e, s - Stata Export
+    pyautogui.press('alt')
+    pyautogui.press('e')
+    pyautogui.press('s')
+    # extentions - ana, dct, do
+    screen_wait(r".\screenshot\DE_stata_export.png")
+
+    export_file = build_fname(xml_file, destination,'csv')
+    save_file(export_file)
+    pass
+
+def save_file(fname):
+    # Write the name in the File Name field
+    pyautogui.write(fname) 
+    pyautogui.press('enter')
+    #Confirm overwrite existing file
+    if screen_wait(r".\screenshot\DE_save_as.png"):
+        pyautogui.press('y')
+    return
+
+def main():
     subprocess.Popen(r"Z:\SLMS_CTU_IT_SOFTWARE\Uat\MACRO Data Exporter\MacroDataExporterV4.0 (slmscctusql01)\MacroDataExporter.exe")
-    if screen_wait(r".\screenshot\DE_login_button.png")==None:
-        exit
+    screen_wait(r".\screenshot\DE_login_button.png")  
 
     pyautogui.click(r".\screenshot\DE_trusted_connection.png")
     pyautogui.click(r".\screenshot\DE_login_button.png")
 
     with open('.\DE_xml_list.csv', mode='rt') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
-        for row in csv_reader:
-            print(row.keys())
-            print(row['trial'],row['xml'], row['frequency'], row['export_format'], row['destination'])
-            
-            weekday=datetime.datetime.now().strftime("%a").lower()
-
+        weekday=datetime.datetime.now().strftime("%a").lower()
+        for row in csv_reader:           
             if row['frequency'].strip() != '' and row['frequency'].lower().find(weekday) == -1:
                 continue
 
-            if screen_wait(r".\screenshot\DE_file.png")==None:
-                exit
+            screen_wait(r".\screenshot\DE_file.png")
+
             open_xml(row['xml'])
-            if screen_wait(r".\screenshot\DE_global_options.png")==None:
-                exit
+            screen_wait(r".\screenshot\DE_global_options.png")
+
             if row['export_format'].lower()=='csv':
                 export_csv(row['xml'], row['destination'])
-            pyautogui.hotkey('alt','f4')
 
-    # alt, e, s - Stata Export
-    # alt, e, e - CSV Export
+            pyautogui.hotkey('alt','f4') # Exit the application
+    return
 
+
+
+if __name__ == '__main__':
+    main()
