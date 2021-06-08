761a7b190e95d8af31d7a9809bed2118b552f1fb
Garrie Powers <g.powers@ucl.ac.uk>
2021-06-08 14:26:35 +0100

    Added requirements.txt. Updated .gitignore

--- auto_de.py
+++ auto_de.py
@@ -11,6 +11,8 @@ import logging
 pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 1
 
+logging.basicConfig(filename='.\_logs\DE'+datetime.datetime.now().strftime("_%G-%m-%d")+'.log', filemode='w',format='%(asctime)s - %(message)s',level=logging.INFO)
+
 def screen_wait(ssname, required=True, search_time=10):
     result = pyautogui.locateOnScreen(ssname,minSearchTime=search_time, grayscale=True)
     if result == None and required==True:
@@ -29,12 +31,14 @@ def open_xml(xml_file):
 
 def export_csv(xml_file, destination):
     # alt, e, e - CSV Export
+    logging.info('Export to CSV')
     pyautogui.press('alt')
     pyautogui.press('e')
     pyautogui.press('e')
     screen_wait(r".\screenshot\DE_csv_export.png")
 
     export_file = build_fname(xml_file, destination)
+    logging.info(f"Filename:{export_file}")
     save_file(export_file)
     return
 
@@ -47,11 +51,13 @@ def build_fname(xml_file, destination, ext=''):
     return fname
 
 def export_stata(xml_file, destination):
+    # alt, e, s - Export STATA
+    logging.info('Export to STATA')
     pyautogui.press('alt')
     pyautogui.press('e')
     pyautogui.press('s')
     export_file = build_fname(xml_file, destination)
-    
+    logging.info(f"Filename:{export_file}")
     while screen_wait(r".\screenshot\DE_stata_export.png", required=False):
         save_file(export_file)
     return
@@ -67,39 +73,45 @@ def save_file(fname):
     return
 
 def main():
-    subprocess.Popen(r"Z:\SLMS_CTU_IT_SOFTWARE\Uat\MACRO Data Exporter\MacroDataExporterV4.0 (slmscctusql01)\MacroDataExporter.exe")
-    screen_wait(r".\screenshot\DE_login_button.png")  
+    try:
+        logging.info('Launch Data Exporter')
+        subprocess.Popen(r"Z:\SLMS_CTU_IT_SOFTWARE\Uat\MACRO Data Exporter\MacroDataExporterV4.0 (slmscctusql01)\MacroDataExporter.exe")
+        logging.info('Login to Data Exporter')
+        screen_wait(r".\screenshot\DE_login_button.png")  
+        pyautogui.click(r".\screenshot\DE_trusted_connection.png")
+        pyautogui.click(r".\screenshot\DE_login_button.png")
+    except:
+        logging.exception("Exception occurred")
 
-    pyautogui.click(r".\screenshot\DE_trusted_connection.png")
-    pyautogui.click(r".\screenshot\DE_login_button.png")
-
-    with open('.\DE_xml_list.csv', mode='rt') as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter=',')
-        weekday=datetime.datetime.now().strftime("%a").lower()
-        for row in csv_reader:
-            #Frequency is used to indicate if a export is to be run on the same day each week (WED)
-            if row['frequency'].strip() != '' and row['frequency'].lower().find(weekday) == -1:
-                continue
-
-            try:
-                ssname=r".\screenshot\DE_file.png"
-                screen_wait(ssname)
-            except pyautogui.ImageNotFoundException:
-                logging.error(f'{ssname} not detected.')
-
-
-            open_xml(row['xml'])
-            screen_wait(r".\screenshot\DE_global_options.png")
-
-            if row['export_format'].lower()=='csv':
-                export_csv(row['xml'], row['destination'])
-                continue
-
-            if row['export_format'].lower()=='stata':
-                export_stata(row['xml'], row['destination'])
-                continue
+    try:
+        logging.info('Read list of Data Exporter XML files')
+        with open('.\DE_xml_list.csv', mode='rt') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=',')
+            weekday=datetime.datetime.now().strftime("%a").lower()
             
-        pyautogui.hotkey('alt','f4') # Exit the application
+            for row in csv_reader:
+                #Frequency is used to indicate if a export is to be run on the same day each week (WED)
+                if row['frequency'].strip() != '' and row['frequency'].lower().find(weekday) == -1:
+                    continue
+                
+                logging.info(f"Processing:{row['xml']}")
+                screen_wait(r".\screenshot\DE_file.png", search_time=30)
+
+                open_xml(row['xml'])
+                screen_wait(r".\screenshot\DE_global_options.png")
+
+                if row['export_format'].lower()=='csv':
+                    export_csv(row['xml'], row['destination'])
+                    continue
+
+                if row['export_format'].lower()=='stata':
+                    export_stata(row['xml'], row['destination'])
+                    continue
+
+            logging.info('Exit Data Exporter')
+            pyautogui.hotkey('alt','f4') # Exit the application
+    except:
+        logging.exception("Exception occurred")
     return
 
 if __name__ == '__main__':
